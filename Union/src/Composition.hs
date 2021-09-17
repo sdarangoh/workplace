f96d0a0029b0324b0bfdf23405ada02045b622e7
sdarangoh <sdarangoh@eafit.edu.co>
2021-09-17 14:59:29 -0500

    a lot of trys

--- Union/src/Composition.hs
+++ Union/src/Composition.hs
@@ -1,7 +1,37 @@
-module Composition
- ( union
- ) where
+module Composition where
 
+
+import Numeric.Natural 
 import Language.Mira.FA.Types
-union :: FA Int -> FA Int -> Int
-union fa1 fa2 = 42
+import Language.Mira.FA.Lib
+import Data.Set (Set)
+import GHC.List
+import GHC.Real
+import GHC.Num
+import qualified GHC.Num as Num
+import qualified GHC.Real as Real
+import qualified Data.Set as Set
+import qualified GHC.List as List
+
+estad :: FA Natural -> FA Natural -> Set Natural
+estad ( MkFA estados1 _ _ _ ) (MkFA estados2 _ _ _ )
+  = Set.union estados1 (Set.union (Set.difference estados2 
+  ( Set.intersection estados1 estados2)) (s estados1 estados2)) 
+
+func :: FA a -> FA a -> (Set (Move a), Set (Move a))
+func (MkFA _ moves _ _ ) (MkFA _ moves1 _ _) = (moves , moves1)
+
+start :: FA Natural -> Natural
+start ( MkFA _ _ init _) = init
+
+final :: Ord a => FA a -> FA a -> Set a
+final ( MkFA _ _ _ fin1 ) (MkFA _ _ _ fin2 ) = Set.union fin1 fin2 
+
+bound :: Set Natural -> Set Natural -> Natural
+bound a b 
+  = List.maximum [(Set.findMax a), (Set.findMax b)]
+ 
+s :: Set Natural -> Set Natural -> Set Natural
+s a b = Set.fromList [ (Num.fromInteger (Real.toInteger x)) 
+      + (bound a b) | x <- [1.. Set.size(Set.intersection a b)]] 
+
--- Union/src/Main.hs
+++ Union/src/Main.hs
@@ -4,10 +4,10 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 
 import Composition
-
+import Numeric.Natural
 import Language.Mira.FA.Types
 
-fa1 :: FA Int
+fa1 :: FA Natural
 
 fa1 = MkFA 
   { statesFA = Set.fromList [1,2,3]
@@ -16,5 +16,16 @@ fa1 = MkFA
   , finalStatesFA = Set.fromList [2] 
   }
 
+fa2 :: FA Natural
+
+fa2 = MkFA 
+  { statesFA = Set.fromList [1,2,4,5,3]
+  , movesFA = Set.fromList [ Move 1 'a' 2, Move 2 'a' 5, Move 5 'a' 2, Emove 3 3]
+  , startStateFA = 2
+  , finalStatesFA = Set.fromList [3] 
+  }
+
 main :: IO ()
-main = print $ fa1
+main = print $ estad fa1 fa2
+--(Set.fromList [1,2,3,45,105,98]) (Set.fromList [1,3,5,7,98,90,304])
+
